83f0bd7f508b105b69d19d455c0fc6e5ef2f955c
詹傑宇 <120771768+jieyu714@users.noreply.github.com>
2025-06-06 16:18:09 +0800

    Adding functionality

--- JS/script.js
+++ JS/script.js
@@ -4,48 +4,84 @@ const supabaseUrl = 'https://mdspsmxwitbczvfwzpiu.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kc3BzbXh3aXRiY3p2Znd6cGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczMjMwNDUsImV4cCI6MjA2Mjg5OTA0NX0.2sb9ReFc7T2sqjcTGrzPK_til3XcZVNBgcb4UzGegM4'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-async function importAndShowData() {
+let userData, userError;
+
+async function importData(group) {
 
     const personName = document.getElementById('name').value;
 
-    const { data, error } = await supabase
-        .from("officeMoney")
-        .select('*')
-        .eq('name', personName);
+    if (group == 'sell') {
+        const { data, error } = await supabase
+            .from("officeMoney")
+            .select('*')
+            .eq('buyOrSell', 'Sell')
+            .order('date', { ascending: true });
+        userData = data;
+        userError = error;
+    } else if (group == "buy") {
+        const { data, error } = await supabase
+            .from("officeMoney")
+            .select('*')
+            .eq('buyOrSell', 'Buy')
+            .order('date', { ascending: true });
+        userData = data;
+        userError = error;
+    } else if (group == "all") {
+        const { data, error } = await supabase
+            .from("officeMoney")
+            .select('*')
+            .order('date', { ascending: true });
+        userData = data;
+        userError = error;
+    } else if (group == 'query') {
+        const { data, error } = await supabase
+            .from("officeMoney")
+            .select('*')
+            .eq('name', personName)
+            .order('date', { ascending: true });
+        userData = data;
+        userError = error;
+    }
 
-    if (error) {
-        console.error('資料獲取錯誤:', error);
+    if (userError) {
+        console.error('資料獲取錯誤:', userError);
         return;
     }
-    
-    
+
+    showData();
+}
+
+async function showData() {
     const dataTable = document.getElementById("show");
     const tableHead = dataTable.querySelector("thead");
     const tableBody = dataTable.querySelector("tbody");
     let htmlContent = '';
 
-    if (data.length) {
+    if (userData.length) {
         tableHead.innerHTML = "<tr><th>Name</th><th>Date</th><th>Buy or Sell</th><th>Store</th><th>Items</th><th>Amount</th></tr>";
         
         let sum = 0;
 
-        for (let i = 0; i < data.length; i++) {
+        for (let i = 0; i < userData.length; i++) {
+            let isBold = userData[i].finish ? '<b>' : '';
+            let boldEnd = userData[i].finish ? '</b>' : '';
+
             let tmp = `<tr>
-                        <td>${data[i].name}</td>
-                        <td>${data[i].date}</td>
-                        <td>${data[i].buyOrSell}</td>
-                        <td>${data[i].store}</td>
-                        <td>${data[i].items}</td>
-                        <td>${data[i].amount}</td>
+                        <td>${isBold}${userData[i].name}${boldEnd}</td>
+                        <td>${isBold}${userData[i].date}${boldEnd}</td>
+                        <td>${isBold}${userData[i].buyOrSell}${boldEnd}</td>
+                        <td>${isBold}${userData[i].store}${boldEnd}</td>
+                        <td>${isBold}${userData[i].items}${boldEnd}</td>
+                        <td>${isBold}${userData[i].amount}${boldEnd}</td>
                     </tr>`;
-            
+
             htmlContent += tmp;
             
-            sum += parseInt(data[i].amount) * (data[i].buyOrSell.toLowerCase() == "buy" ? -1 : 1);
+            sum += parseInt(userData[i].amount) * (userData[i].buyOrSell.toLowerCase() == "buy" ? -1 : 1);
         }
 
         let tmp = `<tr>
-                        <td>${personName}</td>
+                        <td></td>
                         <td>total</td>
                         <td>${(sum == 0 ? '' : (sum > 0 ? "Get" : "Pay"))}</td>
                         <td></td>
@@ -64,5 +100,16 @@ async function importAndShowData() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("query").addEventListener("click", importAndShowData);
+    document.getElementById("query").addEventListener("click", function() {
+        importData('query');
+    });
+    document.getElementById("all").addEventListener("click", function() {
+        importData('all');
+    });
+    document.getElementById("sell").addEventListener("click", function() {
+        importData('sell');
+    });
+    document.getElementById("buy").addEventListener("click", function() {
+        importData('buy');
+    });
 });
